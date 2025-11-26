0fd363811ed2c722e31cc98722a9df14d0ab8be7
aaronlitvack-tmu <aaron.litvack@torontomu.ca>
2025-11-25 23:13:58 -0500

    Adding model training. creating new environment to use GPU

--- AER850_Project3_F2025.py
+++ AER850_Project3_F2025.py
@@ -1,17 +1,18 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+from ultralytics import YOLO
 
 img = cv.imread('data/motherboard_image.JPEG')
 imgg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#imgg = cv.medianBlur(imgg, 5)
-ret,thresh = cv.threshold(imgg,90,150,cv.THRESH_BINARY_INV)
+imgg = cv.medianBlur(imgg, 5)
+ret,thresh = cv.threshold(imgg,90,170,cv.THRESH_BINARY_INV)
 #thresh = cv.adaptiveThreshold(imgg, 500, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 111, 3)
 
 cv.namedWindow('Image', cv.WINDOW_NORMAL)
 cv.imshow('Image', thresh)
 cv.waitKey(0)
-cv.imwrite('imgg_thresh.jpg', thresh)
+#cv.imwrite('imgg_thresh.jpg', thresh)
 cv.destroyAllWindows()
 
 contours, hierarchy = cv.findContours(image=thresh, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_NONE)
@@ -34,3 +35,10 @@ cv.namedWindow('Masked Image', cv.WINDOW_NORMAL)
 cv.imshow('Masked Image', masked_image)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+
+
+model = YOLO("yolo11n.pt")
+
+trained = model.train(data="data/data/data/data.yaml", epochs = 100, imgsz = 900, name = "Prj3Mdl")
+
